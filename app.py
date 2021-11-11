dc70b2d8a945facb8bb7c79a8df06c8e086b3c1b
Saurav10729 <Saurav10729@users.noreply.github.com>
2021-11-11 20:57:55 +0545

    base64 conversion by using cv2.imencode method 11/11/2021

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def object_detection_rectangle():
     file = request.files['image']
     image = Image.open(file.stream)
     opencv_image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
-
     w = image.width
     h = image.height
 
     opencv_image = generate_image_rectangle(opencv_image)
-    image2str=pickle.dumps(opencv_image)
+    retval,image2str = cv2.imencode('.jpg',opencv_image)
+    # image2str=pickle.dumps(opencv_image)
     print("function was accessed")
     image_encode = base64.b64encode(image2str).decode()
-    print(image_encode)
+    print(type(opencv_image))
+    print(type(image2str))
     print(type(image_encode))
-
 
     return jsonify({'message': 'success', 'size': [w,h],'image':image_encode})
 
@@ -45,13 +45,15 @@ def object_measurement_circle():
     opencv_image,circle_detected = generate_image_circle(opencv_image)
     print(circle_detected)
 
-    opencv_image2str = pickle.dumps(opencv_image)
+    retval,image2str = cv2.imencode('.jpg',opencv_image)
+    image_encode = base64.b64encode(image2str).decode()
+    # opencv_image2str = pickle.dumps(opencv_image)
     print("function was accessed")
 
-    image1_encode = base64.b64encode(opencv_image2str).decode('ascii')
+    # image1_encode = base64.b64encode(opencv_image2str).decode('ascii')
 
-    print(type(image1_encode))
-    return jsonify({'message': 'success', 'size': [w,h],'image':base64.b64encode(opencv_image2str).decode('ascii'),'no_of_circles':circle_detected})
+    # print(type(image1_encode))
+    return jsonify({'message': 'success', 'size': [w,h],'image':image_encode,'no_of_circles':circle_detected})
 
 
 @app.route('/colordetection', methods =['POST'])
@@ -59,22 +61,19 @@ def color_detection():
     file = request.files.get('image')
     request_data = request.data
     print(request_data)
-    # print(type(request_data))
-    # x= request_data["x-coord"]
-    # # x_value = int(x)
-    # # print(x_value)
-    # y = request_data["y-coord"]
-    # # y_value = int(y)
-    # # print(y_value)
-    # image = Image.open(file.stream)
-    # opencv_image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
-    #
-    # B,G,R = opencv_image [x_value,y_value]
-    # color_name = get_color_name(R,G,B)
-    #
-    # return jsonify({'msg': 'success', 'colorname':color_name, 'R-value':R,'G-value':G,'B-value':B})
-    return jsonify({'msg': 'success'})
+    print(type(request_data))
+    x = request_data['x-coord']
+    x_value = int(x)
+    print(x)
+    y = request_data['y-coord']
+    print(y)
+    image = Image.open(file.stream)
+    opencv_image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
 
+    B,G,R = opencv_image [x,y]
+    color_name = get_color_name(R,G,B)
+
+    return jsonify({'msg': 'success', 'colorname':color_name, 'R-value':R,'G-value':G,'B-value':B})
     # return "Color Detection feature"
 
 @app.route('/angledetector', methods =['POST'])
--- test_rectangledetection.py
+++ test_rectangledetection.py
@@ -4,7 +4,7 @@ import pickle
 import base64
 import json
 
-url = "http://aa57-103-10-31-50.ngrok.io/object_measurement_rectangle"
+url = "http://240d-103-10-31-50.ngrok.io/object_measurement_rectangle"
 
 
 my_img = {'image': open('Testimages\laptopwitharuco.jpg', 'rb')}
@@ -12,15 +12,21 @@ my_img = {'image': open('Testimages\laptopwitharuco.jpg', 'rb')}
 r = requests.post(url, files=my_img)
 if( r.ok):
     print("response recieved")
-    js = r.json()
+js = r.json()
 
 imagereturn= js['image']
 print(type(imagereturn))
 imdata = base64.b64decode(imagereturn)
 # print(imdata)
 print(type(imdata))
-image = pickle.loads(imdata)
+# image = pickle.loads(imdata)
 # print(js)
-pilimage =Image.fromarray(image[...,::-1])
-
-pilimage.show()
+# pilimage =Image.fromarray(image[...,::-1])
+# pilimage.show()
+with open('result.jpg','wb') as file:
+    file.write(imdata)
+print("file saved")
+f= open('b64.txt','w')
+f.write(imagereturn)
+f.close()
+print("b64 saved")
