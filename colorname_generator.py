b82277819d808818dfa711d69b80331443733748
Saurav10729 <Saurav10729@users.noreply.github.com>
2022-02-22 14:42:25 +0545

    removing color.csv

--- colorname_generator.py
+++ colorname_generator.py
@@ -2,7 +2,6 @@ import cv2
 import pandas as pd
 
 global cname
-csv_path = 'CSV/colors.csv'
 clicked = False
 r = g = b = x_pos = y_pos = 0
 index = ['color', 'color_name', 'hex', 'R', 'G', 'B']
@@ -10,12 +9,12 @@ df = pd.read_csv(csv_path, names=index, header=None)
 
 
 def get_color_name(R, G, B):
-    global cname
-    minimum = 1000
-    for i in range(len(df)):
-        d = abs(R - int(df.loc[i, 'R'])) + abs(G - int(df.loc[i, 'G'])) + abs(B - int(df.loc[i, 'B']))
-        if d <= minimum:
-            minimum = d
-            cname = df.loc[i, 'color_name']
-
-    return cname
+    # global cname
+    # minimum = 1000
+    # for i in range(len(df)):
+    #     d = abs(R - int(df.loc[i, 'R'])) + abs(G - int(df.loc[i, 'G'])) + abs(B - int(df.loc[i, 'B']))
+    #     if d <= minimum:
+    #         minimum = d
+    #         cname = df.loc[i, 'color_name']
+    #
+    return None
